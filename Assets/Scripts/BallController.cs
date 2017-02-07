9703a8d28f4aa60bcd491cbdbd2082c31cdc842f
Victor Yu <victoryu@Victors-MacBook-Pro.local>
2017-02-07 01:34:51 -0500

    first commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float floatSpeed;
+	public float moveSpeed;
+	public Rigidbody rb;
+
+	private bool goalEntered;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		goalEntered = false;
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * moveSpeed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Goal")) 
+		{
+			this.rb.isKinematic = true;
+			goalEntered = true;
+		}
+	}
+
+	void Update () 
+	{
+		if (goalEntered) {
+			this.gameObject.transform.Translate (Vector3.up * floatSpeed * Time.deltaTime, Space.World);
+		}
+
+		// restarts the game
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			Application.LoadLevel(0);
+		}
+
+		// quits the game
+		if (Input.GetKey (KeyCode.Escape) || Input.GetKey (KeyCode.Q))
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Scripts/PlaneTilt.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneTilt : MonoBehaviour {
+
+	public Vector3 currentRot;
+	public float tiltSensitivity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		currentRot = NormalizeAngles (GetComponent<Transform> ().eulerAngles);
+
+		// press right, go clockwise
+		if ((Input.GetAxis ("Horizontal") > 0.2) && currentRot.z >= -10)
+		{
+			this.transform.Rotate (0, 0, tiltSensitivity * Time.deltaTime * -Input.GetAxis("Horizontal"));
+		}
+
+		// press left, go counter-clockwise
+		if ((Input.GetAxis ("Horizontal") < -0.2) && currentRot.z <= 10)
+		{
+			this.transform.Rotate (0, 0, tiltSensitivity * Time.deltaTime * -Input.GetAxis("Horizontal"));
+		}
+
+		// press up, rotate away from game view
+		if (Input.GetAxis ("Vertical") > 0.2 && currentRot.x <= 10) 
+		{
+			this.transform.Rotate (tiltSensitivity * Time.deltaTime * Input.GetAxis("Vertical"), 0, 0);
+		}
+
+		// press downwards, rotate towards game view
+		if (Input.GetAxis ("Vertical") < -0.2 && currentRot.x >= -10) 
+		{
+			this.transform.Rotate (tiltSensitivity * Time.deltaTime * Input.GetAxis("Vertical"), 0, 0);
+		}
+
+	}
+
+	Vector3 NormalizeAngles (Vector3 currentRot) {
+		if (currentRot.x >= 180) 
+		{
+			currentRot.x -= 360;
+		}
+		if (currentRot.y >= 180) 
+		{
+			currentRot.y -= 360;
+		}
+		if (currentRot.z >= 180) 
+		{
+			currentRot.z -= 360;
+		}
+		return currentRot;
+	}
+}
