2465fcf7d417af918f2428ce8e601f7cd0882b47
Victor Yu <victoryu@Victors-MacBook-Pro.local>
2017-02-08 03:32:07 -0500

    Second commit

--- Assets/Scripts/PlaneTilt.cs
+++ Assets/Scripts/PlaneTilt.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlaneTilt : MonoBehaviour {
 
+	public float tiltAngle;
 	public Vector3 currentRot;
 	public float tiltSensitivity;
 
@@ -18,25 +19,25 @@ public class PlaneTilt : MonoBehaviour {
 		currentRot = NormalizeAngles (GetComponent<Transform> ().eulerAngles);
 
 		// press right, go clockwise
-		if ((Input.GetAxis ("Horizontal") > 0.2) && currentRot.z >= -10)
+		if ((Input.GetAxis ("Horizontal") > 0.2) && currentRot.z >= -tiltAngle)
 		{
 			this.transform.Rotate (0, 0, tiltSensitivity * Time.deltaTime * -Input.GetAxis("Horizontal"));
 		}
 
 		// press left, go counter-clockwise
-		if ((Input.GetAxis ("Horizontal") < -0.2) && currentRot.z <= 10)
+		if ((Input.GetAxis ("Horizontal") < -0.2) && currentRot.z <= tiltAngle)
 		{
 			this.transform.Rotate (0, 0, tiltSensitivity * Time.deltaTime * -Input.GetAxis("Horizontal"));
 		}
 
 		// press up, rotate away from game view
-		if (Input.GetAxis ("Vertical") > 0.2 && currentRot.x <= 10) 
+		if (Input.GetAxis ("Vertical") > 0.2 && currentRot.x <= tiltAngle) 
 		{
 			this.transform.Rotate (tiltSensitivity * Time.deltaTime * Input.GetAxis("Vertical"), 0, 0);
 		}
 
 		// press downwards, rotate towards game view
-		if (Input.GetAxis ("Vertical") < -0.2 && currentRot.x >= -10) 
+		if (Input.GetAxis ("Vertical") < -0.2 && currentRot.x >= -tiltAngle) 
 		{
 			this.transform.Rotate (tiltSensitivity * Time.deltaTime * Input.GetAxis("Vertical"), 0, 0);
 		}
---
+++ Assets/Scripts/WindParticles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindParticles : MonoBehaviour {
+
+	public float windForce;
+
+	void OnParticleCollision (GameObject other) {
+		Rigidbody body = other.GetComponent<Rigidbody> ();
+		if (other.gameObject.CompareTag ("Ball")) 
+		{
+			body.AddForce(Vector3.right * windForce);
+		}
+	}
+}
