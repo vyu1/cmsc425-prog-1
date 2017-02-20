0bc14ed62cce2ccf33db159416598864cb674953
Victor Yu <victoryu@Victors-MacBook-Pro.local>
2017-02-20 04:09:17 -0500

    Afraid computer gonna shut off

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,14 +6,33 @@ public class BallController : MonoBehaviour {
 
 	public float floatSpeed;
 	public float moveSpeed;
-	public Rigidbody rb;
+	public float thrustSpeed;
+	public float gridZ;
+	public float gridX;
 
+	public Rigidbody rb;
+	public GameObject hingeCenterPrefab;
+	public GameObject innerWallPrefab;
+	public Transform plane;
+
+	private List<GameObject> wallParentPrefabs = new List<GameObject> ();
 	private bool goalEntered;
 
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody> ();
 		goalEntered = false;
+
+		for (float z = -4; z <= gridZ; z+=2) {
+			for (float x = -4; x <= gridX; x+=2) {
+				Vector3 pos = new Vector3 (x, 0.1f, z);
+				GameObject obj = Instantiate (hingeCenterPrefab, pos, Quaternion.identity, plane) as GameObject;
+				Vector3 pos2 = new Vector3 (x-0.4f, 0.1f, z);
+				GameObject obj2 = Instantiate (innerWallPrefab, pos2, Quaternion.identity) as GameObject;
+				obj2.transform.parent = obj.transform;
+				wallParentPrefabs.Add (obj);
+			}
+		}
 	}
 
 	void FixedUpdate () 
@@ -51,6 +70,20 @@ public class BallController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.Escape) || Input.GetKey (KeyCode.Q))
 		{
 			Application.Quit();
+		}
+
+		// if the user hits the space bar, the ball jumps.
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			rb.AddForce(0, thrustSpeed, 0, ForceMode.Impulse);
+		}
+
+		float random;
+		foreach (GameObject wallParent in wallParentPrefabs) {
+			random = Random.Range (0.0f, 1.0f);
+			if (random < (Time.deltaTime / 10)) {
+				
+			}
 		}
 	}
 }
